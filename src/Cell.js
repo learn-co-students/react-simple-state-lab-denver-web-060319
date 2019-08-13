c11263d6724a96b72a1e6cc1847bc5d3148c9b2b
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-08-13 09:26:06 -0600

    Practice using state and props
    Update state upon the occurence of an event

---
+++ src/Cell.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class Cell extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      color: props.value
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: '#333'
+    })
+  }
+
+  render(){
+    console.log(this.props)
+    return(
+      <div className="cell" style={{backgroundColor: this.state.color}} onClick={this.handleClick}>
+
+      </div>
+    )
+  }
+}
